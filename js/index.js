1ea97bff935159b87e9705206c3c91ab67a3bd11
jeffbutlerspringdale <jeff.butler.springdale@gmail.com>
2019-06-17 17:38:27 -0500

    header is working, first piece of main content is, the rest isnt

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br> Is <br>Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -40,3 +40,40 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navs = document.querySelector('nav').children;
+navs [0].textContent = siteContent['nav']['nav-item-1'];
+navs [1].textContent = siteContent['nav']['nav-item-2'];
+navs [2].textContent = siteContent['nav']['nav-item-3'];
+navs [3].textContent = siteContent['nav']['nav-item-4'];
+navs [4].textContent = siteContent['nav']['nav-item-5'];
+navs [5].textContent = siteContent['nav']['nav-item-6'];
+
+const navsAll = document.querySelectorAll('nav a');
+navsAll.forEach((link) => {
+  link.style.color = 'green';
+})
+
+let ctaText = document.querySelector('.cta-text h1');
+ctaText.innerHTML = siteContent['cta']['h1'];
+
+let ctaButton = document.querySelector('.cta-text button');
+ctaButton.innerHTML = siteContent['cta']['button'];
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+
+let featuresHeader = document.querySelector('.text-content h4');
+featuresHeader.innerHTML = siteContent['main-content']['features-h4'];
+
+let featuresContent = document.querySelector('.text-content p');
+featuresContent.innerHTML = siteContent['main-content']['features-content'];
+
+// let aboutHeader = document.querySelector('.text-content h4');
+// aboutHeader.innerHTML = siteContent['main-content']['about-h4'];
+
+// let aboutContent = document.querySelector('.text-content p');
+// aboutContent.innerHTML = siteContent['main-content']['about-content'];
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
